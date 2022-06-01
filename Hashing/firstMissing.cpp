8b3d852b3d6884c6017386518971e2d128c5a8a6
Hritik Raushan <official.ihritik@gmail.com>
2022-06-01 14:11:24 +0530

    commit

---
+++ Hashing/firstMissing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto it:nums)
+        {
+            s.insert(it);
+        }
+        
+        int smallest = 1;
+        
+        for(auto it : s)
+        {
+            if(!(s.find(smallest) != s.end()))
+            {
+                return smallest;
+            }
+            else
+            {
+                smallest++;
+            }
+        }
+        return smallest;
+    }
+};
