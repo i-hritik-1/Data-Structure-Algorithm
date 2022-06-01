1fe5d2dbf870e590a0423c730476cf6debc7b09b
Manish <official.iamanish@gmail.com>
2022-06-01 14:03:58 +0530

    changes made

--- LEETCODE/Daily Challenge JUNE/runningSum.cpp
+++ LEETCODE/Daily Challenge JUNE/runningSum.cpp
@@ -7,13 +7,22 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
-        vector<int> ans(n);
-        for(int i=0; i<nums.size(); i++)
+
+        // vector<int> ans(n);
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     sum = sum + nums[i];
+        //     ans[i] = sum;
+        // }
+        // return ans;
+
+        // Without using and extra space
+
+        for(int i = 1; i < n; i++)
         {
-            sum = sum + nums[i];
-            ans[i] = sum;
+            nums[i] += nums[i-1];
         }
-        return ans;
+        return nums;
     }
 };
 
