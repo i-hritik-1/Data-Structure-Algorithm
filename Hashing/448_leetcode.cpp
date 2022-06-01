77634659ee0605b9421b1c2ddd4d9569b6c8d443
Hritik Raushan <official.ihritik@gmail.com>
2022-06-01 14:12:21 +0530

    kardo

--- Hashing/448_leetcode.cpp
+++ Hashing/448_leetcode.cpp
@@ -1,20 +1,20 @@
-class Solution {
-public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> ans;
-        set<int> s;
-        for(auto it:nums)
-        {
-            s.insert(it);
-        }
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         vector<int> ans;
+//         set<int> s;
+//         for(auto it:nums)
+//         {
+//             s.insert(it);
+//         }
         
-        for(int i=1; i<=nums.size(); i++)
-        {
-            if(s.find(i) == s.end())
-            {
-                ans.push_back(i);
-            }
-        }
-        return ans;
-    }
-};
+//         for(int i=1; i<=nums.size(); i++)
+//         {
+//             if(s.find(i) == s.end())
+//             {
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+//     }
+// };
--- Hashing/firstMissing.cpp
+++ Hashing/firstMissing.cpp
@@ -1,25 +1,25 @@
-class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto it:nums)
-        {
-            s.insert(it);
-        }
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         unordered_set<int> s;
+//         for(auto it:nums)
+//         {
+//             s.insert(it);
+//         }
         
-        int smallest = 1;
+//         int smallest = 1;
         
-        for(auto it : s)
-        {
-            if(!(s.find(smallest) != s.end()))
-            {
-                return smallest;
-            }
-            else
-            {
-                smallest++;
-            }
-        }
-        return smallest;
-    }
-};
+//         for(auto it : s)
+//         {
+//             if(!(s.find(smallest) != s.end()))
+//             {
+//                 return smallest;
+//             }
+//             else
+//             {
+//                 smallest++;
+//             }
+//         }
+//         return smallest;
+//     }
+// };
