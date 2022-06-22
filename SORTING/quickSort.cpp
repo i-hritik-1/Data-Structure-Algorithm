fc2b6e48ded7092ffc465935f9c1fa28b7e8761e
Hritik Raushan <official.ihritik@gmail.com>
2022-06-22 11:22:40 +0530

    sort

---
+++ SORTING/quickSort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void quickSort(int arr[], int n)
+{
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    
+    }
+
+    quickSort(arr,n);
+    
+
+    return 0;
+}
