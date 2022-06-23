c442680b49758ab1ea9ef0c41aba8753ce7f0558
Hritik Raushan <official.ihritik@gmail.com>
2022-06-23 09:11:21 +0530

    commit quick

--- SORTING/quickSort.cpp
+++ SORTING/quickSort.cpp
@@ -1,24 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void quickSort(int arr[], int n)
-{
+class Solution{
+    public:
+    void QuickSort(int arr[], int low, int high)
+    {
+        if(low<high)
+        {
+        int p = partition(arr,low,high);
+        QuickSort(arr,low,p-1);
+        QuickSort(arr,p+1,high);
+        }
+    }
 
-}
+    public:
+    int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for(int j = low;  j<high; j++)
+        {
+            if(arr[j] < pivot)
+            {
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
+        swap(arr[i+1],arr[high]);
+
+        return i+1;
+
+    }
+
+};
 
 int main()
 {
     int n;
     cin>>n;
     int arr[n];
+
     for(int i=0; i<n; i++)
     {
         cin>>arr[i];
-    
     }
+    Solution obj;
+    obj.QuickSort(arr,0,n);
 
-    quickSort(arr,n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     
-
     return 0;
 }
